4282933e22d2a420781c4dc59265c2ec3b68ca43
Yusuke Soeta <king1@soedayuuyuu-no-MacBook-Pro.local>
2017-01-24 00:50:32 +0900

    Create Gayabot

--- main.go
+++ main.go
@@ -1,28 +1,69 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
+    "github.com/line/line-bot-sdk-go/linebot"
+    "fmt"
+    "time"
+    "math/rand"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+    port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+    router := gin.New()
+    router.Use(gin.Logger())
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
 
-	router.Run(":" + port)
+    router.POST("/hook", func(c *gin.Context) {
+        client := &http.Client{Timeout: time.Duration(15 * time.Second)}
+        bot, err := linebot.New(<Channel Secret>, <Channel Access Token>, linebot.WithHTTPClient(client))
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        received, err := bot.ParseRequest(c.Request)
+
+        for _, event := range received {
+            if event.Type == linebot.EventTypeMessage {
+                switch message := event.Message.(type) {
+                case *linebot.TextMessage:
+                    source := event.Source
+                    if source.Type == linebot.EventSourceTypeRoom {
+                        if resMessage := getResMessage(message.Text); resMessage != "" {
+                            postMessage := linebot.NewTextMessage(resMessage)
+                            if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
+                                log.Print(err)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    router.Run(":" + port)
+}
+
+
+func getResMessage(reqMessage string) (message string) {
+    resMessages := []string{"ねぇ〜〜","ねぇ、聞いて！！","おいっ！","タバコ吸ってくるわ","二郎行こうぜ〜","カビパンばかうめぇ"}
+
+    rand.Seed(time.Now().UnixNano())
+    if rand.Intn(5) == 0 {
+        if math := rand.Intn(4); math != 3 {
+            message = resMessages[math];
+        } else {
+            message = reqMessage + "じゃねーよ！櫻井だよ！"
+        }
+    }
+    return
 }
