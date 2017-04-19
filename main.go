65781d33db5ba606a36314a6f4e664f5ea36ec8d
soeyusuke <s1240231@gmail.com>
2017-04-19 15:33:34 +0900

    test

--- main.go
+++ main.go
@@ -97,17 +97,13 @@ func main() {
                           fmt.Println(weather_data.Weather[0].Main)
                        if _, err = bot.ReplyMessage(event.ReplyToken,
                          linebot.NewTextMessage("現在の天気をお知らせします。"),
-                         linebot.NewTextMessage("天気 : "+ weather_data.Weather[0].Main)),
+                         linebot.NewTextMessage("天気 : "+ weather_data.Weather[0].Main),
+                         linebot.NewImageMessage("https://openweathermap.org/img/w/"+weather_data.Weather[0].Icon+".png", "http://openweathermap.org/img/w/"+weather_data.Weather[0].Icon+".png"),
                          linebot.NewTextMessage("気温 : " + fmt.Sprintf("%.2f", (weather_data.Info.Temp - 273.15))),
-                         linebot.NewTextMessage("湿度 : " + fmt.Sprintf("%.2f", (weather_data.Info.Humidity))).Do(); err != nil {
+                         linebot.NewTextMessage("湿度 : " + fmt.Sprintf("%.2f", (weather_data.Info.Humidity))),
+                         ).Do(); err != nil {
                            log.Print(err)
                        }
-                        //  linebot.NewImageMessage("https://openweathermap.org/img/w/"+weather_data.Weather[0].Icon+".png", "http://openweathermap.org/img/w/"+weather_data.Weather[0].Icon+".png"),
-                        //  linebot.NewTextMessage("気温 : " + fmt.Sprintf("%.2f", (weather_data.Info.Temp - 273.15))),
-                        //  linebot.NewTextMessage("湿度 : " + fmt.Sprintf("%.2f", (weather_data.Info.Humidity))),
-                      //    ).Do(); err != nil {
-                      //      log.Print(err)
-                      //  }
                      default:
                       if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("位置情報を入力してください")).Do(); err != nil{
                          log.Print(err)
